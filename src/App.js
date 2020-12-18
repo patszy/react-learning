22b3ee55382c65e0866053de3f2c0e4246790fec
patszy <patszy97@interia.pl>
2020-12-18 15:11:07 +0100

    React State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import AppHeader from './components/AppHeader';
 import ContactsList from './components/ContactsList';
+import Counter from './components/Counter';
+import InputName from './components/InputName';
 
 function App() {
   return (
     <div className="App">
-      <AppHeader />
         <main>
-          <ContactsList />
+          <InputName />
         </main>
     </div>
   );
--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-export default function ContactItem({login, name, department}){
-    return(
-        <li>
-            <div>
-                <h4>{name}</h4>
-                <p>{department}</p>
-            </div>
-        </li>
-    )
+export default class ContectItem extends React.Component{
+    render() {
+        const {login, name, department} = this.props;
+
+        return(
+            <li>
+                <div>
+                    <h4>{name}</h4>
+                    <p>{department}</p>
+                </div>
+            </li>
+        )
+    }
 }
